bea9aae22414e14d06056e1978872e170566a881
Valentina <Vallitriya@gmail.com>
2023-10-19 19:22:12 +0300

    connecting to the mongodb

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 
-dotenv.config()
+dotenv.config();
 const app = express();
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
-app.use(morgan("common"))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+const PORT = process.env.PORT || 9000;
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
