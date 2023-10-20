765bd95490f3a07b51b01d5ebaef7b877e73d373
Valentina <Vallitriya@gmail.com>
2023-10-20 20:11:57 +0300

    connecting route '/kpi'

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import  kpisRouter from "./routes/kpi.js"
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+application.use("/kpi", kpisRouter)
+
+const PORT = process.env.PORT || 9000;
+async function startServer() {
+	try {
+	  await mongoose.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true
+	  });
+	  console.log('Connected to MongoDB');
+	  
+	  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+	} catch (error) {
+	  console.error(`${error} did not connect`);
+	}
+  }
+  
+  startServer();
