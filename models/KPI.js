3b001ab2c62a3f6513c886739249f1339838a9bc
Valentina <Vallitriya@gmail.com>
2023-10-21 23:28:07 +0300

    defining the dayschema

--- models/KPI.js
+++ models/KPI.js
@@ -4,6 +4,23 @@ import { loadType } from "mongoose-currency";
 const Schema = mongoose.Schema;
 loadType(mongoose);
 
+const daySchema = new Schema(
+	{
+		date: String,
+		revenue: {
+			type: mongoose.Types.Currency,
+			currency: "USD",
+			get: (v) => v / 100,
+		},
+		expenses: {
+			type: mongoose.Types.Currency,
+			currency: "USD",
+			get: (v) => v / 100,
+		},
+	},
+	{ toJSON: { getters: true } }
+);
+
 const KPISchema = new Schema(
 	{
 		totalProfit: {
