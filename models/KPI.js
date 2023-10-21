84110391aaa3f995a6a62be55d534281a6cc35d6
Valentina <Vallitriya@gmail.com>
2023-10-21 23:19:15 +0300

    defining the kpischema

--- models/KPI.js
+++ models/KPI.js
@@ -4,6 +4,31 @@ import { loadType } from "mongoose-currency";
 const Schema = mongoose.Schema;
 loadType(mongoose);
 
+const KPISchema = new Schema({
+	totalProfit: {
+		type: mongoose.Types.Currency,
+		currency: "USD",
+		get: (v) => v / 100,
+	},
+	totalRevenue: {
+		type: mongoose.Types.Currency,
+		currency: "USD",
+		get: (v) => v / 100,
+	},
+	totaExpenses: {
+		type: mongoose.Types.Currency,
+		currency: "USD",
+		get: (v) => v / 100,
+	},
+	expenseByCategory: {
+		type: Map,
+		of: {
+			type: mongoose.Types.Currency,
+			currency: "USD",
+			get: (v) => v / 100,
+		},
+	},
+});
 
 const KPI = mongoose.model("KPI", KPISchema);
 
