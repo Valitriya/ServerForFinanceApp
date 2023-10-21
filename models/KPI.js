8498bc1870918205e2f8850d6d3147d059d3ba5d
Valentina <Vallitriya@gmail.com>
2023-10-21 23:22:31 +0300

    added monthlydata to kpischema to store monthly data

--- models/KPI.js
+++ models/KPI.js
@@ -4,31 +4,34 @@ import { loadType } from "mongoose-currency";
 const Schema = mongoose.Schema;
 loadType(mongoose);
 
-const KPISchema = new Schema({
-	totalProfit: {
-		type: mongoose.Types.Currency,
-		currency: "USD",
-		get: (v) => v / 100,
-	},
-	totalRevenue: {
-		type: mongoose.Types.Currency,
-		currency: "USD",
-		get: (v) => v / 100,
-	},
-	totaExpenses: {
-		type: mongoose.Types.Currency,
-		currency: "USD",
-		get: (v) => v / 100,
-	},
-	expenseByCategory: {
-		type: Map,
-		of: {
+const KPISchema = new Schema(
+	{
+		totalProfit: {
 			type: mongoose.Types.Currency,
 			currency: "USD",
 			get: (v) => v / 100,
 		},
+		totalRevenue: {
+			type: mongoose.Types.Currency,
+			currency: "USD",
+			get: (v) => v / 100,
+		},
+		totaExpenses: {
+			type: mongoose.Types.Currency,
+			currency: "USD",
+			get: (v) => v / 100,
+		},
+		expenseByCategory: {
+			type: Map,
+			of: {
+				type: mongoose.Types.Currency,
+				currency: "USD",
+				get: (v) => v / 100,
+			},
+		},
+		monthlyData: [],
 	},
-});
+);
 
 const KPI = mongoose.model("KPI", KPISchema);
 
