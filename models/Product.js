9539544088b53ae1dbb3031ed3eb026b1b255037
Valentina <Vallitriya@gmail.com>
2023-11-11 23:47:27 +0300

    adding ProductSchema

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,21 @@ import { loadType } from "mongoose-currency";
 const Schema = mongoose.Schema;
 loadType(mongoose);
 
-const currencyCongig = {
+const currencyConfig = {
 	type: mongoose.Types.Currency,
 	currency: "USD",
 	get: (v) => v / 100,
 };
-
+const ProductSchema = new Schema({
+	price: { ...currencyConfig },
+	expense: { ...currencyConfig },
+	transactions: [
+	  {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Transaction",
+	  },
+	],
+  }, { timestamps: true, toJSON: { getters: true } });
 
 
 export default mongoose.model("Product");
