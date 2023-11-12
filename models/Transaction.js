95672c756ff83f25d7f4236f815e56150e610d9c
Valentina <Vallitriya@gmail.com>
2023-11-12 21:11:33 +0300

    reusing ProductSchema in TransactionSchema

---
+++ models/Transaction.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { loadType } from "mongoose-currency";
+
+const Schema = mongoose.Schema;
+loadType(mongoose);
+
+const currencyConfig = {
+	type: mongoose.Types.Currency,
+	currency: "USD",
+	get: (v) => v / 100,
+};
+const TransactionSchema = new Schema({
+	price: { ...currencyConfig },
+	expense: { ...currencyConfig },
+	transactions: [
+	  {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Transaction",
+	  },
+	],
+  }, { timestamps: true, toJSON: { getters: true } });
+
+
+export default mongoose.model("Transaction", TransactionSchema);
